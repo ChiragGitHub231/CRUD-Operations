1703ae6530f425b35ee1a4f6933b67f8ffa5bb95
ChiragGitHub231 <chiragbhundiya433@gmail.com>
2024-06-27 18:43:52 +0530

    Complete Update Record Functionality

--- crud_operations/app/views.py
+++ crud_operations/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import User
 
 # Create your views here.
@@ -25,3 +25,43 @@ def insertData(request):
 
     # render & redirect to index.html page
     return render(request, "index.html")
+
+def updateData(request,id):
+    # Filter the user object and stores to d variable
+    d=User.objects.get(id=id)
+    context={"d":d}
+
+    # if form is submitted then action request is handled
+    if request.method=="POST":
+        # gets & stores the form data to variables
+        name=request.POST['name']
+        email=request.POST['email']
+        contact=request.POST['contact']
+        address=request.POST['address']
+        gender=request.POST['gender']
+
+        # Filter the record by id
+        edit=User.objects.get(id=id)
+
+        # Update the details of that record
+        edit.name=name
+        edit.email=email
+        edit.contact=contact
+        edit.address=address
+        edit.gender=gender
+
+        # Save the updated record
+        edit.save()
+
+        # Once record is updated then it is redirected to index.html page
+        return redirect("/")
+
+    # render & redirect to edit.html page
+    return render(request, "edit.html", context)
+
+def deleteData(request,id):
+    # Return all the user objects and stores to data variable
+    data=User.objects.all()
+    context={"data":data}
+
+    return render(request, "index.html", context)
