21ea2cf5d7b42d06b5070500590cf4b9705a5041
ChiragGitHub231 <chiragbhundiya433@gmail.com>
2024-06-27 18:10:56 +0530

    Completed Fetch Records Functionality

--- crud_operations/app/views.py
+++ crud_operations/app/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    # Return all the user objects and stores to data variable
+    data=User.objects.all()
+    context={"data":data}
+
+    return render(request, "index.html", context)
 
 def insertData(request):
+    # if form is submitted then action request is handled
     if request.method=="POST":
+        # gets & stores the form data to variables
         name=request.POST.get('name')
         email=request.POST.get('email')
         contact=request.POST.get('contact')
         address=request.POST.get('address')
         gender=request.POST.get('gender')
 
-        print(name, email, contact, address, gender)
+        # Create an instance of User Model with form data and save the data
+        query=User(name=name, email=email, contact=contact, address=address, gender=gender)
+        query.save()
 
+    # render & redirect to index.html page
     return render(request, "index.html")
