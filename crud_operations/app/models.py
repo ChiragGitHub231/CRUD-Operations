b069bcd0165793f7ae925f2f68808286de8f061b
ChiragGitHub231 <chiragbhundiya433@gmail.com>
2024-06-27 17:28:17 +0530

    Create & Migrate User Model

--- crud_operations/app/models.py
+++ crud_operations/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name=models.CharField(max_length=30,blank=False,null=False)
+    email=models.EmailField()
+    contact=models.IntegerField()
+    address=models.CharField(max_length=50,blank=False,null=False)
+    gender=models.CharField(max_length=10,blank=False,null=False)
+
+    def __str__(self):
+        return self.name
--- crud_operations/app/views.py
+++ crud_operations/app/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def insertData(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        contact=request.POST.get('contact')
+        address=request.POST.get('address')
+        gender=request.POST.get('gender')
+
+        print(name, email, contact, address, gender)
+
+    return render(request, "index.html")
